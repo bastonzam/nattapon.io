c24f552d20a69904ad9ccfbee21c7f62b95b686f
NChangchai <NChangchai@kerrylogistics.com>
2021-08-14 19:01:30 +0700

    push git

---
+++ pages/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-row >
+    <v-col cols="12" sm="6" md="6" v-for="p in project" :key="p.slug">
+      <v-card >
+        <v-card-title class="headline">
+          {{p.title}}
+        </v-card-title>
+        <v-card-text>
+          {{p.description}}
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+            color="primary"
+            nuxt
+            :to="{name:'project-id',params:{id:p.slug}}"
+          >
+            Show detail of {{p.title}}
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+<script>
+export default {
+  async asyncData({$content}){
+     const projectData = await $content('project').fetch();
+     return {
+       project:projectData
+     }
+  },
+  mounted(){
+    console.log(this.project)
+  }
+}
+</script>
---
+++ pages/project/_id.vue
@@ -0,0 +1,36 @@
+<template>
+    <v-row>
+        <v-col col="12" sm="6" >
+             <v-card >
+                <v-img
+                class="white--text align-end"
+                height="200px"
+                :src="project.image"
+                />
+                <v-card-title class="headline">
+                {{project.title}}
+                </v-card-title>
+                <v-card-text>
+                {{project.description}}
+                </v-card-text>
+
+                <hr>
+                <nuxt-content :document="project"></nuxt-content>
+                
+            </v-card>
+        </v-col>
+    </v-row>
+</template>
+<script>
+export default {
+    async asyncData({$content,params}){
+        const projectData =  await $content('project',params.id).fetch();
+        return {
+            project : projectData
+        }
+    },
+    mounted(){
+        console.log(this.project);
+    }
+}
+</script>
